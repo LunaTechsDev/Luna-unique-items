a1bbd9a55e4d663519213d95bb4878b62e12e27b
Kino  Rose <kinoarose@gmail.com>
2020-08-23 19:13:05 -0400

    Complete unique items for LunaTechs

--- src/LunaUniqueItems.hx
+++ src/LunaUniqueItems.hx
@@ -1,20 +1,74 @@
-import macros.MacroTools;
-import rm.managers.PluginManager;
+import rm.objects.Game_Actor;
+import core.Amaryllis;
+import core.Types.JsFn;
+import rm.objects.Game_Party;
+import rm.types.RPG.BaseItem;
+import rm.Globals;
 import utils.Fn;
 import utils.Comment;
 
-using core.StringExtensions;
-using Std;
+using core.NumberExtensions;
 
 class LunaUniqueItems {
  public static var textSpeed: Int = 2;
 
  public static function main() {
   // Plugin parameters can be include here as an internal call.
-  // MacroTools.includeJsLib("./src/TestPluginParams.js");
+  Comment.title("Game_Party");
 
-  var parameters: Any = PluginManager.parameters("TestPlugin");
-  textSpeed = Fn.getByArrSyntax(parameters, "Text Speed");
-  trace(textSpeed);
+  var gamePartyGainItem: JsFn = Fn.getPrProp(Game_Party, "gainItem");
+  var returningToInventory: Bool;
+  var uniqueItemEmitter = Amaryllis.createEventEmitter();
+
+  var RETURN_ITEM = "returnEquipItem";
+  var RESET_RETURN = "resetReturn";
+
+  uniqueItemEmitter.on(RETURN_ITEM, () -> {
+   returningToInventory = true;
+  });
+
+  uniqueItemEmitter.on(RESET_RETURN, () -> {
+   returningToInventory = false;
+  });
+
+  var oldChangeEquip: JsFn = Fn.getPrProp(Game_Actor, "changeEquip");
+  Fn.setPrProp(Game_Actor, "changeEquip", (slotId: Int, item: BaseItem) -> {
+   oldChangeEquip.call(Fn.self, slotId, item);
+   uniqueItemEmitter.emit(RESET_RETURN);
+  });
+
+  var oldTradeItem: JsFn = Fn.getPrProp(Game_Actor, "tradeItemWithParty");
+  Fn.setPrProp(Game_Actor, "tradeItemWithParty",
+   (newItem: BaseItem, oldItem: BaseItem) -> {
+    if (~/<\s*unique\s*/ig.match(oldItem.note)) {
+     uniqueItemEmitter.emit(RETURN_ITEM);
+    }
+    oldTradeItem.call(Fn.self, newItem, oldItem);
+   });
+
+  Fn.setPrProp(Game_Party, "gainItem",
+
+   (item: BaseItem, amount: Int, includeEquip: Bool) -> {
+    var partySelf: Game_Party = Fn.self;
+    // Party Has Item but no actor has it equipped
+    if (~/<\s*unique\s*/ig.match(item.note)) {
+     if (partyHasItem(item, true)) {
+      if (returningToInventory) {
+       gamePartyGainItem.call(Fn.self, item, amount.clamp(0, 1), includeEquip);
+      } else {
+       gamePartyGainItem.call(Fn.self, item, amount.clamp(-1, 0), includeEquip);
+      }
+     } else if (!partyHasItem(item, true)) {
+      gamePartyGainItem.call(Fn.self, item, amount.clamp(-1, 1), includeEquip);
+     }
+    } else {
+     gamePartyGainItem.call(Fn.self, item, amount, includeEquip);
+    }
+   });
+ }
+
+ public static function partyHasItem(item: BaseItem,
+   includeEquip: Bool): Bool {
+  return Globals.GameParty.hasItem(item, includeEquip);
  }
 }
--- src/LunaUniqueItemsParams.js
+++ src/LunaUniqueItemsParams.js
@@ -1,24 +1,40 @@
 /*:
-@author Test
-@plugindesc An extension to the core Message Window functionality
-to support Visual Novels <TestPlugin>.
+@author LunaTechs - Kino
+@plugindesc Allows you to tag database items as unique to prevent item stacking
+<LunaUnqItm>.
 
-@param Text Speed 
-@desc The speed at which characters will be rendered
-@default 2
+@target MV MZ
 
 @help
 Version: 1.00
-Version Log:
-Now you can change the text speed at will using escape characters
-inside the window.
-Example: \\TS[30] updates the text speed to super slow 30.
-Note: The [30] will appear in the editor, but not in game.
+Introduction:
+This plugin allows you to make an item unique and prevent
+stacking of that same time.
+
+
 
 Instructions:
-You set your text speed in the plugin menu.
-This is the speed that the characters will be drawn at.
+Insert the notetag <unique> in that item's notebox
+to trigger the effect.
 
 Contact me via forums; username: Kino.
 Hope this plugin helps and enjoy!
+
+MIT License
+Copyright (c) 2020 LunaTechsDev
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
 */
